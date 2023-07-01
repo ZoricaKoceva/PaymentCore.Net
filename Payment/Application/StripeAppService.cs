df21bc55491c18a17bd1fdb7e98c50f9ddbc44e7
Zorica Koceva <zorica.koceva@sia-soft.dev>
2023-07-01 02:17:05 +0200

    Add controller, Stripe Infrastructure and changes on settings

--- Payment/Application/StripeAppService.cs
+++ Payment/Application/StripeAppService.cs
@@ -23,9 +23,32 @@ namespace Payment.Application
         /// <param name="customer">Stripe Customer</param>
         /// <param name="cancellationToken">Cancellation Token</param>
         /// <returns>Stripe Customer</returns>
-        public Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken cancellationToken)
+        public async Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            TokenCreateOptions tokenOptions = new TokenCreateOptions
+            {
+                Card = new TokenCardOptions
+                {
+                    Name = customer.Name,
+                    Number = customer.CreditCard.CardNumber,
+                    ExpYear = customer.CreditCard.ExpirationYear,
+                    ExpMonth = customer.CreditCard.ExpirationMonth,
+                    Cvc = customer.CreditCard.CVC
+                }
+            };   
+                
+            Token stripeToken = await _tokenService.CreateAsync(tokenOptions, null, cancellationToken);
+
+            CustomerCreateOptions customerOptions = new CustomerCreateOptions
+            {
+                Name = customer.Name,
+                Email = customer.Email,
+                Source = stripeToken.Id
+            };
+
+            Customer createdCustomer = await _customerService.CreateAsync(customerOptions, null, cancellationToken);
+
+            return new StripeCustomer(createdCustomer.Name, createdCustomer.Email, createdCustomer.Id);
         }
 
         /// <summary>
@@ -35,9 +58,29 @@ namespace Payment.Application
         /// <param name="payment">Stripe Payment</param>
         /// <param name="cancellationToken">Cancellation Token</param>
         /// <returns><Stripe Payment/returns>
-        public Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken cancellationToken)
+        public async Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // Set the options for the payment we would like to create at Stripe
+            ChargeCreateOptions paymentOptions = new ChargeCreateOptions
+            {
+                Customer = payment.CustomerId,
+                ReceiptEmail = payment.ReceiptEmail,
+                Description = payment.Description,
+                Currency = payment.Currency,
+                Amount = payment.Amount
+            };
+
+            // Create the payment
+            var createdPayment = await _chargeService.CreateAsync(paymentOptions, null, cancellationToken);
+
+            // Return the payment to requesting method
+            return new StripePayment(
+              createdPayment.CustomerId,
+              createdPayment.ReceiptEmail,
+              createdPayment.Description,
+              createdPayment.Currency,
+              createdPayment.Amount,
+              createdPayment.Id);
         }
     }
 }
---
+++ Payment/Controllers/StripeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Payment.Contracts;
+using Payment.Models.Stripe;
+
+namespace Payment.Controllers
+{
+    [Route("api/[controller]")]
+    public class StripeController : Controller
+    {
+        private readonly IStripeAppService _stripeService;
+
+        public StripeController(IStripeAppService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+        [HttpPost("customer/Add")]
+        public async Task<ActionResult<StripeCustomer>> AddStripeCustomer([FromBody] AddStripeCustomer customer, CancellationToken cancellationToken)
+        {
+            StripeCustomer createdStripeCustomer = await _stripeService.AddStripeCustomerAsync(customer, cancellationToken);
+        
+            return Ok(createdStripeCustomer);
+        }
+
+        [HttpPost("Payment/Add")]
+        public async Task<ActionResult<StripePayment>> AddStripePayment([FromBody] AddStripePayment payment, CancellationToken cancellationToken)
+        {
+            StripePayment createdPayment = await _stripeService.AddStripePaymentAsync(payment, cancellationToken);
+
+            return Ok(createdPayment);
+        }
+
+    }
+}
